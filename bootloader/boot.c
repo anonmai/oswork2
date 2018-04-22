c38d16ef76d40c387cbd34ccb5ac44788e9753e1
Ouyang <4509767770@qq.com>
2018-04-22 21:57:22 +0800

    422

--- bootloader/boot.c
+++ bootloader/boot.c
@@ -4,6 +4,16 @@
 
 void bootMain(void) {
 	/* 加载内核至内存，并跳转执行 */
+	unsigned int sceno = 1;			//设置要读写的磁盘扇区号为1，即第二个磁盘扇区
+	unsigned long va = 0x100000;		//设置要写入的内存地址为0x8c00
+	for(int i=0;i<200;i+=1)
+	{
+		readSect((void*)va, sceno);
+		sceno+=1;
+	}
+	
+	asm volatile("jmp 0x100000");		//跳转到刚刚写入代码的位置执行
+	while(1);						//无限循环
 }
 
 void waitDisk(void) { // waiting for disk
--- kernel/main.c
+++ kernel/main.c
@@ -4,6 +4,12 @@
 
 void kEntry(void) {
 
+	asm volatile(        
+		"movl $((80*3+11)*2), %edi\n\t" 
+        "movb $33, %al\n\t"                   
+        "movw %ax, %gs:(%edi)\n\t"              
+		"1:      hlt\n\t"
+		"jmp 1b"          );
 	initSerial();// initialize serial port
 	initIdt(); // initialize idt
 	initIntr(); // iniialize 8259a
