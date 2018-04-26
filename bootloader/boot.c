25a753195134b9224173b927e188256ec39119bd
Ouyang <4509767770@qq.com>
2018-04-26 13:36:50 +0800

    426

--- bootloader/boot.c
+++ bootloader/boot.c
@@ -1,19 +1,28 @@
 #include "boot.h"
+#include <string.h>
 
 #define SECTSIZE 512
 
 void bootMain(void) {
 	/* 加载内核至内存，并跳转执行 */
-	unsigned int sceno = 1;			//设置要读写的磁盘扇区号为1，即第二个磁盘扇区
-	unsigned long va = 0x100000;		//设置要写入的内存地址为0x8c00
-	for(int i=0;i<200;i+=1)
-	{
-		readSect((void*)va, sceno);
+	void (*p_kern)();
+	int SectNum = 200;					//内核代码占200个扇区
+	int KernelSize = SECTSIZE * SectNum;//内核大小512×200*
+	unsigned char buf[KernelSize];		//用数组装下内核代码
+	unsigned int sceno = 1;				//设置要读写的磁盘扇区号为1，即第二个磁盘扇区
+	for(int i=0;i<200;i++){
+		readSect(buf + i * SECTSIZE , sceno);
 		sceno+=1;
 	}
-	
-	asm volatile("jmp 0x100000");		//跳转到刚刚写入代码的位置执行
-	while(1);						//无限循环
+	struct ELFHeader *elf = (void *)buf;
+	struct ProgramHeader *ph = (void *)elf + elf->phoff;
+	for(int i = elf->phnum; i > 0; --i){
+		memcpy((void *)ph->vaddr, buf + ph->off, ph->filesz);
+		memset((void *)ph->vaddr + ph->filesz, 0, ph->memsz -ph->filesz);
+		ph = (void *)ph + elf->phentsize;
+	}
+	p_kern = (void *)elf->entry;		//空函数指向内核代码入口地址
+	p_kern();							//跳转到入口地址执行
 }
 
 void waitDisk(void) { // waiting for disk
