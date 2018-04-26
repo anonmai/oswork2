9da5f25564c9db2362b5ad46e722721eef41cc70
Ouyang <4509767770@qq.com>
2018-04-26 16:09:19 +0800

    完成printf和syscallhandle

--- kernel/kernel/irqHandle.c
+++ kernel/kernel/irqHandle.c
@@ -26,6 +26,25 @@ void irqHandle(struct TrapFrame *tf) {
 
 void syscallHandle(struct TrapFrame *tf) {
 	/* 实现系统调用*/
+	for(int i = 0; i < tf->edx; ++i){
+		char temp = *(char *)(tf->ecx+i);
+		if(temp != '\n'){
+			*(char *)(p) = temp;
+			*(char *)(p+1) = 0x0c;
+			oneline += 2;
+			p += 2;
+			if(oneline == 160){
+				line ++;
+				oneline = 0;
+			}
+		}
+		else{
+			line++;
+			oneline = 0;
+			p = oxb8000 + line *160;
+		}
+	}
+	tf->eax = tf->edx;
 }
 
 void GProtectFaultHandle(struct TrapFrame *tf){
